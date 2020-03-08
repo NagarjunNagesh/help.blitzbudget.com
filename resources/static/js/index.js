039672dbfe6289347f96fc4c777f660336c311be
Arjun <nagarjun_nagesh@outlook.com>
2020-03-08 18:18:08 +0100

    index js search

--- resources/static/js/index.js
+++ resources/static/js/index.js
@@ -169,8 +169,8 @@
 
 	// Search Article focus out
 	document.getElementById('searchArticle').addEventListener('focusout', (event) => {
-			document.getElementById('searchArticleDD').classList.add('fadeOut');
-			document.getElementById('searchArticleDD').classList.remove('fadeInDown');
+		document.getElementById('searchArticleDD').classList.add('fadeOut');
+		document.getElementById('searchArticleDD').classList.remove('fadeInDown');
 	});
 
 	// Dispatch click event
