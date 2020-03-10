c310bd390d7879379446fa12c79ca22b792f0583
Arjun <nagarjun_nagesh@outlook.com>
2020-03-10 21:18:17 +0100

    add sub categories for categories

--- resources/static/js/data/categoryInfo.js
+++ resources/static/js/data/categoryInfo.js
@@ -8,11 +8,35 @@ window.categoryInfo = [
 
 	{
 		"categoryName": "Getting Started",
-		"dataUrl": "/getting-started/"
+		"dataUrl": "/getting-started/",
+		"subCategory": [
+			{
+				"title": "About BlitzBudget",
+				"url": "/about-blitzbudget/"
+			},
+			{
+				"title": "Financial Planning You Can Do With BlitzBudget",
+				"url": "/financial-planning-you-can-do-with-blitzbudget/"
+			},
+			{
+				"title": "Open My Account",
+				"url": "/open-my-account/"
+			},
+			{
+				"title": "What You Need To Get Started",
+				"url": "/what-you-need-to-get-started/"
+			}
+		]
 	},
 	{
 		"categoryName": "Budget",
-		"dataUrl": "/budget/"
+		"dataUrl": "/budget/",
+		"subCategory": [
+			{
+				"title": "Compensate Budget",
+				"url": "/compensate-budget/"
+			}
+		]
 	},
 	{
 		"categoryName": "Transactions",
@@ -24,7 +48,51 @@ window.categoryInfo = [
 	},
 	{
 		"categoryName": "Financial Accounts",
-		"dataUrl": "/financial-accounts/"
+		"dataUrl": "/financial-accounts/",
+		"subCategory": [
+			{
+				"title": "Synchronized Bank Account",
+				"url": "/synchronized-bank-account/"
+			},
+			{
+				"title": "Unsynchronized Bank Account",
+				"url": "/unsynchronized-bank-account/"
+			}
+		]
+	},
+	{
+		"categoryName": "Settings",
+		"dataUrl": "/settings/",
+		"subCategory": [
+			{
+				"title": "Updating Your Notification Settings",
+				"url": "/notification-settings/"	
+			},
+			{
+				"title": "Update Email And Password",
+				"url": "/update-email-and-password/"	
+			},
+			{
+				"title": "Delete Your BlitzBudget Account",
+				"url": "/delete-your-blitzbudget-account/"	
+			},
+			{
+				"title": "Bugs, Feedback, And Feature Request",
+				"url": "/bugs-feedback-and-feature-request/"	
+			},
+			{
+				"title": "BlitzBudget For Alexa",
+				"url": "/blitzbudget-for-alexa/"	
+			},
+			{
+				"title": "Keyboard Shortcuts",
+				"url": "/keyboard-shortcuts/"	
+			},
+			{
+				"title": "Syncing With Mobile",
+				"url": "/syncing-with-mobile/"	
+			}
+		]
 	}
 
 ];
