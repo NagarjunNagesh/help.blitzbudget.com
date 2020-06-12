e1646ca93e412e13861367f66f3010a0e15d862e
Minnu Ashok Kumar <minnuashokkumar@Minnus-MacBook-Air.local>
2020-06-12 17:33:05 +0200

    Bracket compatibility

--- resources/static/js/data/categoryInfo.js
+++ resources/static/js/data/categoryInfo.js
@@ -1,179 +1,178 @@
 /**
-*
-* Category Information
-*
-**/
-
+ *
+ * Category Information
+ *
+ **/
 window.categoryInfo = [
-	{
-		"categoryName": "Getting Started",
-		"dataUrl": "/getting-started/",
-		"description": "Total control over your money in a few clicks.",
-		"subCategory": [			
-			{
-				"title": "Getting Started Step 1: Set your goals",
-				"url": "/set-your-goals/"
+    {
+        "categoryName": "Getting Started",
+        "dataUrl": "/getting-started/",
+        "description": "Total control over your money in a few clicks.",
+        "subCategory": [
+            {
+                "title": "Getting Started Step 1: Set your goals",
+                "url": "/set-your-goals/"
 			},
-			{
-				"title": "Getting Started Step 2: Budget your money",
-				"url": "/budget-your-money/"
+            {
+                "title": "Getting Started Step 2: Budget your money",
+                "url": "/budget-your-money/"
 			},
-			{
-				"title": "Getting Started Step 3: Add your investments",
-				"url": "/add-your-investments/"
+            {
+                "title": "Getting Started Step 3: Add your investments",
+                "url": "/add-your-investments/"
 			},
-			{
-				"title": "Getting Started Step 4: Add your financial accounts",
-				"url": "/add-financial-accounts/"
+            {
+                "title": "Getting Started Step 4: Add your financial accounts",
+                "url": "/add-financial-accounts/"
 			},
-			{
-				"title": "Getting Started: What's next?",
-				"url": "/whats-next/"
+            {
+                "title": "Getting Started: What's next?",
+                "url": "/whats-next/"
 			}
 
 		]
 	},
-	{
-		"categoryName": "Budget",
-		"dataUrl": "/budget/",
-		"description": "Plan what you spend & automate your financial plan.",
-		"subCategory": [
-			{
-				"title": "Add a Budget",
-				"url": "/add-a-budget/"
+    {
+        "categoryName": "Budget",
+        "dataUrl": "/budget/",
+        "description": "Plan what you spend & automate your financial plan.",
+        "subCategory": [
+            {
+                "title": "Add a Budget",
+                "url": "/add-a-budget/"
 			},
-			{
-				"title": "Compensate Budget",
-				"url": "/compensate/"
+            {
+                "title": "Compensate Budget",
+                "url": "/compensate/"
 			},
-			{
-				"title": "Delete a Budget",
-				"url": "/delete-a-budget/"
+            {
+                "title": "Delete a Budget",
+                "url": "/delete-a-budget/"
 			}
 		]
 	},
-	{
-		"categoryName": "Transactions",
-		"dataUrl": "/transactions/",
-		"description": "Make sure every spending is accounted for and tracked.",
-		"subCategory": [
-			{
-				"title": "Adding Transactions",
-				"url": "/adding-transactions/"
+    {
+        "categoryName": "Transactions",
+        "dataUrl": "/transactions/",
+        "description": "Make sure every spending is accounted for and tracked.",
+        "subCategory": [
+            {
+                "title": "Adding Transactions",
+                "url": "/adding-transactions/"
 			},
-			{
-				"title": "File-Based Import",
-				"url": "/file-based-import/"
+            {
+                "title": "File-Based Import",
+                "url": "/file-based-import/"
 			},
-			{
-				"title": "Sort By",
-				"url": "/sort-by/"
+            {
+                "title": "Sort By",
+                "url": "/sort-by/"
 			},
-			{
-				"title": "Changing a Transaction's Category",
-				"url": "/changing-a-transactions-category/"
+            {
+                "title": "Changing a Transaction's Category",
+                "url": "/changing-a-transactions-category/"
 			},
-			{
-				"title": "Changing a Budget",
-				"url": "/changing-a-budget/"
+            {
+                "title": "Changing a Budget",
+                "url": "/changing-a-budget/"
 			},
-			{
-				"title": "Edit Notes",
-				"url": "/edit-notes/"
+            {
+                "title": "Edit Notes",
+                "url": "/edit-notes/"
 			},
-			{
-				"title": "Change the Account For a Transaction",
-				"url": "/change-the-account/"
+            {
+                "title": "Change the Account For a Transaction",
+                "url": "/change-the-account/"
 			},
-			{
-				"title": "Export Selected File Format",
-				"url": "/export-selected-file-format/"
+            {
+                "title": "Export Selected File Format",
+                "url": "/export-selected-file-format/"
 			},
-			{
-				"title": "Delete Transactions",
-				"url": "/delete-transactions/"
+            {
+                "title": "Delete Transactions",
+                "url": "/delete-transactions/"
 			}
 		]
 	},
-	{
-		"categoryName": "Goals",
-		"dataUrl": "/goals/",
-		"description": "Planning to buy save your money for something worth saving?."
+    {
+        "categoryName": "Goals",
+        "dataUrl": "/goals/",
+        "description": "Planning to buy save your money for something worth saving?."
 	},
-	{
-		"categoryName": "Financial Accounts",
-		"dataUrl": "/financial-accounts/",
-		"description": "Credit cards and debit cards are easily tracked with financial accounts.",
-		"subCategory": [
-			{
-				"title": "Synchronized Bank Account",
-				"url": "/synchronized-bank-account/"
+    {
+        "categoryName": "Financial Accounts",
+        "dataUrl": "/financial-accounts/",
+        "description": "Credit cards and debit cards are easily tracked with financial accounts.",
+        "subCategory": [
+            {
+                "title": "Synchronized Bank Account",
+                "url": "/synchronized-bank-account/"
 			},
-			{
-				"title": "Unsynchronized Bank Account",
-				"url": "/unsynchronized-bank-account/"
+            {
+                "title": "Unsynchronized Bank Account",
+                "url": "/unsynchronized-bank-account/"
 			}
 		]
 	},
-	{
-		"categoryName": "Wallets",
-		"dataUrl": "/wallets/",
-		"description": "Wanna share your wallet with your friends? Do you have bank accounts accross multiple currencies?.",
-		"subCategory": [
-			{
-				"title": "Add wallet",
-				"url": "/add-wallet/"
+    {
+        "categoryName": "Wallets",
+        "dataUrl": "/wallets/",
+        "description": "Wanna share your wallet with your friends? Do you have bank accounts accross multiple currencies?.",
+        "subCategory": [
+            {
+                "title": "Add wallet",
+                "url": "/add-wallet/"
 			},
-			{
-				"title": "Modify wallet",
-				"url": "/modify-wallet/"
+            {
+                "title": "Modify wallet",
+                "url": "/modify-wallet/"
 			},
-			{
-				"title": "Delete wallet",
-				"url": "/delete-wallet/"
+            {
+                "title": "Delete wallet",
+                "url": "/delete-wallet/"
 			},
-			{
-				"title": "Choose wallet",
-				"url": "/choose-wallet/"
+            {
+                "title": "Choose wallet",
+                "url": "/choose-wallet/"
 			}
 		]
 	},
-	{
-		"categoryName": "Miscellaneous",
-		"dataUrl": "/miscellaneous/",
-		"description": "Tweak your preferences, notifications and more.",
-		"subCategory": [
-			{
-				"title": "Updating Your Notification Settings",
-				"url": "/notification-settings/"	
+    {
+        "categoryName": "Miscellaneous",
+        "dataUrl": "/miscellaneous/",
+        "description": "Tweak your preferences, notifications and more.",
+        "subCategory": [
+            {
+                "title": "Updating Your Notification Settings",
+                "url": "/notification-settings/"
 			},
-			{
-				"title": "Update Email And Password",
-				"url": "/update-email-and-password/"	
+            {
+                "title": "Update Email And Password",
+                "url": "/update-email-and-password/"
 			},
-			{
-				"title": "Delete Your BlitzBudget Account",
-				"url": "/delete-your-blitzbudget-account/"	
+            {
+                "title": "Delete Your BlitzBudget Account",
+                "url": "/delete-your-blitzbudget-account/"
 			},
-			{
-				"title": "Reset Your BlitzBudget Account",
-				"url": "/resetting-your-account/"	
+            {
+                "title": "Reset Your BlitzBudget Account",
+                "url": "/resetting-your-account/"
 			},
-			{
-				"title": "Bugs, Feedback, And Feature Request",
-				"url": "/bugs-feedback-and-feature-request/"	
+            {
+                "title": "Bugs, Feedback, And Feature Request",
+                "url": "/bugs-feedback-and-feature-request/"
 			},
-			{
-				"title": "BlitzBudget For Alexa",
-				"url": "/blitzbudget-for-alexa/"	
+            {
+                "title": "BlitzBudget For Alexa",
+                "url": "/blitzbudget-for-alexa/"
 			},
-			{
-				"title": "Keyboard Shortcuts",
-				"url": "/keyboard-shortcuts/"	
+            {
+                "title": "Keyboard Shortcuts",
+                "url": "/keyboard-shortcuts/"
 			},
-			{
-				"title": "Syncing With Mobile",
-				"url": "/syncing-with-mobile/"	
+            {
+                "title": "Syncing With Mobile",
+                "url": "/syncing-with-mobile/"
 			}
 		]
 	}
--- resources/static/js/plugins/animateOnScroll.js
+++ resources/static/js/plugins/animateOnScroll.js
@@ -1,118 +1,117 @@
-"use strict";
 (function scopeWrapper($) {
 
-  // get the element to animate
-  let fI = document.getElementsByClassName('fI');
-  let fIR = document.getElementsByClassName('fIR');
-  let fIU = document.getElementsByClassName('fIU');
-  let fIL = document.getElementsByClassName('fIL');  
-  let fID = document.getElementsByClassName('fID');  
-  
+    // get the element to animate
+    let fI = document.getElementsByClassName('fI');
+    let fIR = document.getElementsByClassName('fIR');
+    let fIU = document.getElementsByClassName('fIU');
+    let fIL = document.getElementsByClassName('fIL');
+    let fID = document.getElementsByClassName('fID');
 
-  // listen for scroll event and call animate function
-  document.addEventListener('scroll', animate);
 
-  // Check if any of the elements are in view
-  animate();
+    // listen for scroll event and call animate function
+    document.addEventListener('scroll', animate);
 
-  // animate element when it is in view
-  function animate() {
+    // Check if any of the elements are in view
+    animate();
 
-    for(let i = 0, len = fI.length; i < len; i++) {
-      let children = fI[i];
-      // is element in view?
-      if (inView(children)) {
-        // Convert children to classList
-        // element is in view, add class to element
-        let childrenCL = children.classList;
-        childrenCL.add('fadeIn');
-        childrenCL.remove('fI');
-      }
+    // animate element when it is in view
+    function animate() {
+
+        for (let i = 0, len = fI.length; i < len; i++) {
+            let children = fI[i];
+            // is element in view?
+            if (inView(children)) {
+                // Convert children to classList
+                // element is in view, add class to element
+                let childrenCL = children.classList;
+                childrenCL.add('fadeIn');
+                childrenCL.remove('fI');
+            }
+        }
+
+
+        for (let i = 0, len = fIR.length; i < len; i++) {
+            let children = fIR[i];
+            // is element in view?
+            if (inView(children)) {
+                // Fade in right for element
+                let childrenCL = children.classList;
+                childrenCL.add('fadeInRight');
+                childrenCL.remove('fIR');
+            }
+        }
+
+        for (let i = 0, len = fIU.length; i < len; i++) {
+            let children = fIU[i];
+            // is element in view?
+            if (inView(children)) {
+                // Fade in Up for element
+                let childrenCL = children.classList;
+                childrenCL.add('fadeInUp');
+                childrenCL.remove('fIU');
+            }
+        }
+
+
+        for (let i = 0, len = fIL.length; i < len; i++) {
+            let children = fIL[i];
+            // is element in view?
+            if (inView(children)) {
+                // Fade in left for all the children
+                let childrenCL = children.classList;
+                childrenCL.add('fadeInLeft');
+                childrenCL.remove('fIL');
+            }
+        }
+
+
+        for (let i = 0, len = fID.length; i < len; i++) {
+            let children = fID[i];
+            // is element in view?
+            if (inView(children)) {
+                // Fade in left for all the children
+                let childrenCL = children.classList;
+                childrenCL.add('fadeInDown');
+                childrenCL.remove('fID');
+            }
+        }
+
+        // Recalculate the fadeable elements
+        fI = document.getElementsByClassName('fI');
+        fIR = document.getElementsByClassName('fIR');
+        fIU = document.getElementsByClassName('fIU');
+        fIL = document.getElementsByClassName('fIL');
+        fID = document.getElementsByClassName('fID');
+
+        if (isEmpty(fIL) && isEmpty(fIU) && isEmpty(fIR) && isEmpty(fI) && isEmpty(fID)) {
+            // Remove event listener
+            document.removeEventListener('scroll', animate);
+        }
     }
 
-
-    for(let i = 0, len = fIR.length; i < len; i++) {
-      let children = fIR[i];
-      // is element in view?
-      if(inView(children)) {
-        // Fade in right for element
-        let childrenCL = children.classList;
-        childrenCL.add('fadeInRight');
-        childrenCL.remove('fIR');
-      }
-    }
-
-    for(let i = 0, len = fIU.length; i < len; i++) {
-      let children = fIU[i];
-      // is element in view?
-      if(inView(children)) {
-        // Fade in Up for element
-        let childrenCL = children.classList;
-        childrenCL.add('fadeInUp');
-        childrenCL.remove('fIU');
-      }
-    }
-
-
-    for(let i = 0, len = fIL.length; i < len; i++) {
-      let children = fIL[i];
-      // is element in view?
-      if(inView(children)) {
-        // Fade in left for all the children
-        let childrenCL = children.classList;
-        childrenCL.add('fadeInLeft');
-        childrenCL.remove('fIL');
-      }
-    }
-
-
-    for(let i = 0, len = fID.length; i < len; i++) {
-      let children = fID[i];
-      // is element in view?
-      if(inView(children)) {
-        // Fade in left for all the children
-        let childrenCL = children.classList;
-        childrenCL.add('fadeInDown');
-        childrenCL.remove('fID');
-      }
-    }
-
-    // Recalculate the fadeable elements
-    fI = document.getElementsByClassName('fI');
-    fIR = document.getElementsByClassName('fIR');
-    fIU = document.getElementsByClassName('fIU');
-    fIL = document.getElementsByClassName('fIL');
-    fID = document.getElementsByClassName('fID');
-
-    if(isEmpty(fIL) && isEmpty(fIU) && isEmpty(fIR) && isEmpty(fI) && isEmpty(fID)) {
-      // Remove event listener
-      document.removeEventListener('scroll', animate);
-    }
-  }
-  
-}(jQuery)); 
+}(jQuery));
 
 // check if element is in view
 function inView(element) {
 
-  if(isEmpty(element)) {
-    return;
-  }
+    if (isEmpty(element)) {
+        return;
+    }
 
-  // get window height
-  var windowHeight = window.innerHeight;
-  // get number of pixels that the document is scrolled
-  var scrollY = window.scrollY || window.pageYOffset;
-  var elementHeight = element.clientHeight;  
-  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
-  var scrollPosition = scrollY + windowHeight;
-  // get element position (distance from the top of the page to the bottom of the element)
-  var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
-  
-  // is scroll position greater than element position? (is element in view?)
-  if (scrollPosition > elementPosition) {
-    return true;
-  }
-  
-  return false;
+    // get window height
+    var windowHeight = window.innerHeight;
+    // get number of pixels that the document is scrolled
+    var scrollY = window.scrollY || window.pageYOffset;
+    var elementHeight = element.clientHeight;
+    // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+    var scrollPosition = scrollY + windowHeight;
+    // get element position (distance from the top of the page to the bottom of the element)
+    var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
+
+    // is scroll position greater than element position? (is element in view?)
+    if (scrollPosition > elementPosition) {
+        return true;
+    }
+
+    return false;
 }
