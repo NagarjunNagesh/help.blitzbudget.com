eadf4afd0dceb5bf73408f9fd0a73fb8a58f7126
Minnu Ashok Kumar <minnuashokkumar@Minnus-MacBook-Air.local>
2020-06-22 14:47:56 +0200

    translation

--- resources/static/js/core/translator.js
+++ resources/static/js/core/translator.js
@@ -28,7 +28,7 @@ function translatePage(locale) {
 }
 
 function getLanguage() {
-    let languagespreferred = ["en", "es"];
+    let languagespreferred = ["en", "es", "fr", "ja", "pt", "ru", "zh"];
     let lang = navigator.languages ? navigator.languages[0] : navigator.language;
     lang = lang.substr(0, 2);
     lang = includesStr(languagespreferred, lang) ? lang : "en";
